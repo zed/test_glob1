8853681195c983be1e99cb45d0ff6e42a931a389
zed <arn.zart+zed@gmail.com>
2010-10-27 02:44:43 +0400

    switched to distutils, unittest
    all tests pass

---
+++ README.txt
@@ -0,0 +1,15 @@
+`test_glob1` - test `glob.glob1()` availability on different Python versions.
+
+USAGE: $ tox
+
+To install `tox <http://codespeak.net/tox/>`_, type: ``$ pip install tox``
+
+python2.4, python2.5, python2.6, python2.7, python3.0, python3.1,
+python3.2, pypy, jython executables should be in the PATH.
+
+See ``tox.ini`` for the list of tested Python environment. 
+
+Files are licensed under the MIT License. See the file ``MIT-LICENSE.txt``
+for details.
+
+The latest version is at XXX
--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import os
 
-try: import setuptools
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-
-from setuptools import setup
+from distutils.core import setup
 
 def read_file(relpath):
     return open(os.path.join(os.path.dirname(__file__), relpath)).read()
 
-_readme_lines = read_file("README.rst").splitlines()
+_readme_lines = read_file("README.txt").splitlines()
 
 CLASSIFIERS = """\
 Development Status :: 4 - Beta
@@ -26,7 +21,7 @@ NAME             = 'test_glob1'
 VERSION          = '0.0.1'
 DESCRIPTION      = _readme_lines[0]
 LONG_DESCRIPTION = "\n".join(_readme_lines[2:])
-URL              = None #XXX
+URL              = "http://stackoverflow.com/questions/3964681/find-all-files-in-directory-with-extension-txt-with-python/3971553#3971553"
 DOWNLOAD_URL     = None #XXX
 LICENSE          = 'MIT'
 CLASSIFIERS      = filter(len, CLASSIFIERS.split('\n'))
@@ -48,8 +43,6 @@ setup(
     maintainer=AUTHOR,
     maintainer_email=AUTHOR_EMAIL,
     platforms=PLATFORMS,
-    tests_require=['nose'], #NOTE: setup.py test might fail on the first attempt
-    test_suite="nose.collector",
     py_modules=[],
     provides=[],
 )
